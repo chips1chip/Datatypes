40a62e961a724786fa0038d1b3921902e2ccecdb
Anjalinegi <anjalinegi26500@gmail.com>
2025-01-29 10:06:57 +0530

    Update DataTypes.java

--- DataTypes.java
+++ DataTypes.java
@@ -1,23 +1,32 @@
 
 
+
 public class  DataTypes{
 
     public static void main(String[] args) {
+
         int number = 10;
-        System.out.println(number);
+        System.out.println(number);  //int datatype for small integer numbers
+
         String b = "anjali";
-        System.out.println(b);
+        System.out.println(b);  //string datatype for collection of characters
+
         double doub = 255.67898D;
-        System.out.println(doub);
+        System.out.println(doub);  //double datatype for long decimal numbers
+
         char a = '#';
-        System.out.println(a);
+        System.out.println(a);  //char datatype for a single character
+
         boolean bool = true;
-        System.out.println(bool);
+        System.out.println(bool);  //boolean datatype for true of false statement
+
         long l = 35676286332L;
-        System.out.println(l);
+        System.out.println(l);  //long datatype for long integer numbers
+
         short s = 3654;
-        System.out.println(s);
+        System.out.println(s);  //short datatype for short integer numbers
+ 
         float f = 36.54F;
-        System.out.println(f);
+        System.out.println(f);  //float datatype for small decimal numbers
     }
 }
