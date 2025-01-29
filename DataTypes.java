2471e6011bab96258bc28da8b5159098f57c57a6
Anjalinegi <anjalinegi26500@gmail.com>
2025-01-29 10:06:42 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,23 @@
+
+
+public class  DataTypes{
+
+    public static void main(String[] args) {
+        int number = 10;
+        System.out.println(number);
+        String b = "anjali";
+        System.out.println(b);
+        double doub = 255.67898D;
+        System.out.println(doub);
+        char a = '#';
+        System.out.println(a);
+        boolean bool = true;
+        System.out.println(bool);
+        long l = 35676286332L;
+        System.out.println(l);
+        short s = 3654;
+        System.out.println(s);
+        float f = 36.54F;
+        System.out.println(f);
+    }
+}
